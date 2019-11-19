25dff70096e27fd6b55bf74802eef6a6cef02a68
wittenator <wittenator@yahoo.de>
2019-11-19 18:57:21 +0100

    Open Tensorboard automatically

--- start.sh
+++ start.sh
@@ -7,12 +7,19 @@ finish()
 }
 trap finish SIGINT
 
+xo () 
+{ 
+    for var in "$@"; do
+        xdg-open "$var";
+    done
+}
+
 CURRENT_UID=$(id -u):$(id -g) docker-compose up --build -d
 
 time=$(date +"%s")
 until docker logs --since $time VAENotebook 2>&1 | grep -m 1 "127.0.0.1"; do sleep 5 ; done
 token=$(docker logs --since $time VAENotebook 2>&1 | grep '127.0.0.1' | grep -m 1 -oP 'token=\K(.*)')
-xdg-open http://localhost:8888/?token=$token
+xo http://localhost:8888/?token=$token http://localhost:6006
 
 while :; do
     sleep 5
