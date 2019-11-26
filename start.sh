69195c32c5ca762c3f689cd2825b386f8c4c9cda
wittenator <wittenator@yahoo.de>
2019-11-26 17:38:46 +0100

    Fixed and cleaned a bit more

--- start.sh
+++ start.sh
@@ -10,6 +10,7 @@ trap finish SIGINT
 xo () 
 { 
     for var in "$@"; do
+    	echo "$var"
         xdg-open "$var";
     done
 }
@@ -19,7 +20,7 @@ CURRENT_UID=$(id -u):$(id -g) docker-compose up --build -d
 time=$(date +"%s")
 until docker logs --since $time VAENotebook 2>&1 | grep -m 1 "127.0.0.1"; do sleep 5 ; done
 token=$(docker logs --since $time VAENotebook 2>&1 | grep '127.0.0.1' | grep -m 1 -oP 'token=\K(.*)')
-xo http://localhost:8888/?token=$token http://localhost:6006
+xo http://127.0.0.1:8888/?token=$token http://127.0.0.1:6006
 
 while :; do
     sleep 5
