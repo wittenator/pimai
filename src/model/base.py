New approach to numerical stability and small fixes for other distributions

src/model/base.py
@@ -9,17 +9,21 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import pandas as pd
 import os
+import math
+
 
 #import hypertools as hyp
 
 
 class Autoencoder(nn.Module):
-    def __init__(self, device, save_dir):
+    def __init__(self, device, save_dir, warmup_method, warmup_period):
         super(Autoencoder, self).__init__()
         self.device = device
         self.save_dir = save_dir
         now = datetime.now()
         current_time = now.strftime("%Y%m%d-%H%M%S")
+        self.warmup_method = warmup_method
+        self.warmup_period = warmup_period
         self.writer = SummaryWriter(log_dir=self.save_dir + 'data/runs/' + current_time)
         self.embeddings = []
         self.embedding_labels = []
@@ -58,6 +62,17 @@ class Autoencoder(nn.Module):
 
         print('\nTest set: Average loss: {:.4f}, Reconstruction error: {}\n'.format(
             test_loss, recon))
+
+    def sigmoid(self, x):
+        return 1 / (1 + math.exp(-x))
+
+    def kld_warmup(self, epoch, epochs):
+        if self.warmup_method == 'none':
+            return 1
+        elif self.warmup_method == 'sigmoid':
+            return self.sigmoid(epoch / (self.warmup_period*0.3))
+        else:
+            return self.sigmoid((epoch % self.warmup_period) / (self.warmup_period*0.3))
 
     def add_embedding(self, loader):
         with torch.no_grad():

src/model/sbvae.py
@@ -7,12 +7,19 @@ from torch.distributions import Uniform
 import numpy as np
 
 from .base import Autoencoder
-from src.util import Distributions
+from src.util.Distributions import Distributions
 
+from enum import Enum
+
+class Distributions(Enum):
+    KUMARASWAMY = "km"
+    GAUSS_LOGIT = "gl"
+    GAMMA_DIST = "gamma"
 
 class SBVAE(Autoencoder):
-    def __init__(self, device, save_dir, k=50, dist=Distributions.KUMARASWAMY):
-        super(SBVAE, self).__init__(device, save_dir)
+    def __init__(self, device, save_dir, warmup_method, warmup_period, k=50, dist=Distributions.KUMARASWAMY):
+        super(SBVAE, self).__init__(device, save_dir, warmup_method, warmup_period)
+        self.name = 'SB-VAE'
         self.k = k
         self.device = device
         self.fc1 = nn.Linear(784, 400)
@@ -34,33 +41,25 @@ class SBVAE(Autoencoder):
         return F.softplus(self.fc21(h1)), F.softplus(self.fc22(h1))
 
     def reparameterize(self, a, b):
-        eps = 10 * torch.finfo(torch.float).eps  # smalles representable number such that 1.0 + eps != 1.0
-        batch_size = a.size()[0]
+        eps = 5 * torch.finfo(torch.float).eps 
+        batch_size = a.size(0)
 
-        uniform_samples = Uniform(torch.tensor([eps]), torch.tensor([1.0 - eps])).rsample(a.size()).squeeze().to(
-            self.device) if self.device.type == 'cpu' else torch.cuda.FloatTensor(a.size(0),
-                                                                                  a.size(1)).uniform_().clamp(eps,
-                                                                                                              1.0 - eps)
-        exp_a = torch.reciprocal(
-            a)  # returns a new tensor with the reciprocal of the elements of input: out_i = 1/input_i
-        exp_b = torch.reciprocal(b)
+        uniform_samples = Uniform(torch.tensor([0.0]), torch.tensor([1.0])).rsample(a.size()).squeeze().to(
+            self.device) if self.device.type == 'cpu' else torch.cuda.FloatTensor(*a.size()).uniform_()
+        exp_a = torch.reciprocal(a.clamp(eps))
+        exp_b = torch.reciprocal(b.clamp(eps))
         # value for Kumaraswamy distribution
         if self.dist == Distributions.KUMARASWAMY:
-            km = (1 - uniform_samples.pow(exp_b) + eps).pow(exp_a)
+            km = ((1 - uniform_samples.pow(exp_b)).clamp(eps, 1-eps)).pow(exp_a)
         elif self.dist == Distributions.GAMMA_DIST:
             # exp(lgamma(a)) == gamma(a) https://discuss.pytorch.org/t/is-there-a-gamma-function-in-pytorch/17122/2
             gamma_func = torch.lgamma(a).exp()
-            km = ((uniform_samples * a * gamma_func).pow(exp_a)) / b
+            km = ((uniform_samples * a * gamma_func).pow(exp_a) * exp_b)
         else:
             std = torch.exp(0.5 * b)
-            eps = torch.randn_like(std)
-            gauss = a + eps * std
-            return 1 / 1 - gauss.exp()
-
-        self.name = 'SB-VAE (K)'
-
-        # no Nans are allowed in the matrix
-        # assert not torch.isnan(km).any().item()
+            eps = torch.randn_like(std) if self.device.type == 'cpu' else torch.cuda.FloatTensor(*a.size()).normal_()
+            km = F.sigmoid(a + eps * std)
+        print(torch.min(km), torch.max(km))
 
         # concatenates the given sequence of seq tensors in the given dimension. All tensors must either have the same shape (except in the concatenating dimension) or be empty.
         cumprods = torch.cat((torch.ones([batch_size, 1], device=self.device), torch.cumprod(1 - km, axis=1)), dim=1)
@@ -81,7 +80,10 @@ class SBVAE(Autoencoder):
         return torch.exp(torch.lgamma(a) + torch.lgamma(b) - torch.lgamma(a + b))
 
     def KLD(self, a, b, prior_alpha, prior_beta):
-        ab = (a * b)
+        eps = 5 * torch.finfo(torch.float).eps 
+        a = a.clamp(eps)
+        a = a.clamp(eps)
+        ab = (a * b) + eps
         kl = 1 / (1 + ab) * self.Beta(1 / a, b)
         kl += 1 / (2 + ab) * self.Beta(2 / a, b)
         kl += 1 / (3 + ab) * self.Beta(3 / a, b)
@@ -107,14 +109,12 @@ class SBVAE(Autoencoder):
 
     def loss_function(self, recon_x, x, a, b, prior_alpha, prior_beta, epoch, epochs):
         self.counter += 1
-        period = 20
         BCE = F.binary_cross_entropy_with_logits(recon_x, x.view(-1, 784), reduction='none')
-        KLD = self.KLD(a, b, prior_alpha, prior_beta)
+        KLD = self.kld_warmup(epoch, epochs) * self.KLD(a, b, prior_alpha, prior_beta)
         self.writer.add_scalar('KLD/train', KLD.sum(), self.counter)
         self.writer.add_scalar('BCE/train', BCE.sum(), self.counter)
 
-        return 60000 / a.size(0) * torch.mean(
-            1 / period * (epoch % period) * KLD.sum(axis=1) + BCE.sum(axis=1))
+        return 60000 / a.size(0) * torch.mean(KLD.sum(axis=1) + BCE.sum(axis=1))
 
     def compute_loss_train(self, data, target, epoch, epochs):
         recon_batch, a, b = self(data)

src/model/sssbvae.py
@@ -7,8 +7,8 @@ import torch.nn.functional as F
 from torch.distributions import Categorical
 
 class SSSBVAE(SBVAE):
-    def __init__(self, device, save_dir, k=50):
-        super(SSSBVAE, self).__init__(device, save_dir, k=k)
+    def __init__(self, device, save_dir, warmup_method, warmup_period, k=50):
+        super(SSSBVAE, self).__init__(device, save_dir, warmup_method, warmup_period, k=k)
 
         self.fc23 = nn.Linear(400, 10)
         self.fc3 = nn.Linear(self.k + 10, 400)
@@ -40,7 +40,7 @@ class SSSBVAE(SBVAE):
         factor = torch.where(y_true != -1, torch.ones(a.size(0), device=self.device),
                              F.binary_cross_entropy(y_batch, eye_batch, reduction='none').sum(axis=(1, 2)))
 
-        term = 1 / period * (epoch % period) * KLD.sum(axis=1) + BCE.sum(axis=1) * factor + y_recon
+        term = kld_warmup(epoch, epochs)* KLD.sum(axis=1) + BCE.sum(axis=1) * factor + y_recon
 
         return 60000 / a.size(0) * (torch.mean(term))
 

src/model/vae.py
@@ -5,8 +5,8 @@ from .base import Autoencoder
 
 
 class VAE(Autoencoder):
-    def __init__(self, device, save_dir, k=20):
-        super(VAE, self).__init__(device, save_dir)
+    def __init__(self, device, save_dir, warmup_method, warmup_period, k=20):
+        super(VAE, self).__init__(device, save_dir, warmup_method, warmup_period)
 
         self.conv1 = nn.Conv2d(1, 32, 3, 1)
         self.conv2 = nn.Conv2d(32, 64, 3, 1)
@@ -20,11 +20,6 @@ class VAE(Autoencoder):
         self.fc4 = nn.Linear(400, 784)
 
     def encode(self, x):
-        # x = self.conv1(x)
-        # x = F.relu(x)
-        # x = self.conv2(x)
-        # x = F.max_pool2d(x, 2)
-        # x = self.dropout1(x)
         x = torch.flatten(x, 1)
         h1 = F.relu(self.fc1(x))
         return self.fc21(h1), self.fc22(h1)
@@ -44,7 +39,7 @@ class VAE(Autoencoder):
         z = self.reparameterize(mu, logvar)
         return self.decode(z), mu, logvar
 
-    def loss_function(self, recon_x, x, mu, logvar):
+    def loss_function(self, recon_x, x, mu, logvar, epoch, epochs):
         self.counter += 1
         BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
 
@@ -52,7 +47,7 @@ class VAE(Autoencoder):
         # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
         # https://arxiv.org/abs/1312.6114
         # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
-        KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+        KLD = self.kld_warmup(epoch, epochs)(-0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp()))
         self.writer.add_scalar('KLD/train', KLD.sum(), self.counter)
         self.writer.add_scalar('BCE/train', BCE.sum(), self.counter)
 
@@ -60,8 +55,8 @@ class VAE(Autoencoder):
 
     def compute_loss_train(self, data, target, epoch, epochs):
         recon_batch, mu, logvar = self(data)
-        return self.loss_function(recon_batch, data, mu, logvar)
+        return self.loss_function(recon_batch, data, mu, logvar, epoch, epochs)
 
     def compute_loss_test(self, data, target, epoch, epochs):
         recon_batch, mu, logvar = self(data)
-        return self.loss_function(recon_batch, data, mu, logvar).item(), recon_batch  # sum up batch loss
+        return self.loss_function(recon_batch, data, mu, logvar, epoch, epochs).item(), recon_batch  # sum up batch loss

train.py
@@ -26,6 +26,10 @@ model.add_argument('--latent-size', type=int, default=50,
                    help='dimensionality of latent variable. Default:%(default)s')
 model.add_argument('--prior-concentration-param', type=float, default=1.,
                    help="the Beta prior's concentration parameter: v ~ Beta(1, alpha0). The larger the alpha0, the deeper the net. Default:%(default)s")
+model.add_argument('--warmup-method', type=str, default='cycle',
+                   help='dimensionality of latent variable. Default:%(default)s')
+model.add_argument('--warmup-period', type=int, default=20,
+                   help='dimensionality of latent variable. Default:%(default)s')
 
 training = p.add_argument_group("Training options")
 training.add_argument('--batch-size', type=int, default=100,
@@ -47,23 +51,23 @@ torch.autograd.set_detect_anomaly(True)
 device, train_loader, test_loader, train_loader_occluded, test_loader_occluded = build_dataset(args.save_dir)
 
 if args.model == 'vae':
-    model = VAE(device, args.save_dir, k=args.latent_size).to(device)
+    model = VAE(device, args.save_dir, args.warmup_method, args.warmup_period, k=args.latent_size).to(device)
 elif args.model == 'sbvae':
-    model = SBVAE(device, args.save_dir, k=args.latent_size).to(device)
+    model = SBVAE(device, args.save_dir, args.warmup_method, args.warmup_period, k=args.latent_size).to(device)
 elif args.model == 'sbvae':
-    model = SSSBVAE(device, args.save_dir, k=args.latent_size).to(device)
+    model = SSSBVAE(device, args.save_dir, args.warmup_method, args.warmup_period, k=args.latent_size).to(device)
 
-optimizer = optim.Adam(model.parameters())
+optimizer = optim.Adam(model.parameters(), lr=0.003)
 model.writer.add_graph(model, next(iter(train_loader))[0].to(device))
 
 
 
-#scheduler = StepLR(optimizer, step_size=1)
+scheduler = StepLR(optimizer, step_size=1)
 epochs = args.max_epoch
 for epoch in range(1, epochs + 1):
     model.trains(device, train_loader if args.model != 'sssbvae' else train_loader_occluded, optimizer, epoch, epochs)
     model.tests(device, test_loader if args.model != 'sssbvae' else test_loader_occluded, epoch, epochs)
-    #scheduler.step()
+    scheduler.step()
     model.add_embedding(test_loader)
 torch.save(model.state_dict(), f'{args.save_dir}data/{args.model}-{args.max_epoch}-{args.latent_size}.pth')
 